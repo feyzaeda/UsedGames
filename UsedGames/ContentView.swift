2e2d5c87a37fc2411ccb34576790f4956dfee5d2
feyzaeda <tfeyzaeda@posta.pau.edu.tr>
2021-12-14 10:06:03 +0300

    Initial Commit

---
+++ UsedGames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsedGames
+//
+//  Created by Feyza Eda TÜRK on 14.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Feyza Eda TÜRK on 14.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
